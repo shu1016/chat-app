ff7f89856b982dd96a7e19696d89ed5cb9972e37
shu1016 <s.a.m-kawai@outlook.jp>
2024-05-30 18:31:02 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
- root to: "messages#index"
- devise_for :users
- resources :users, only: [:edit, :update]
- resources :rooms, only: [:new, :create]
+  devise_for :users
+  root to: "rooms#index"
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
